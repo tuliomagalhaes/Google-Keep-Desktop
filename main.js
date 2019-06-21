36e7581b29041416838e24c2e0fc7ebc6923f58b
Túlio Henrique <tuliohdvp@gmail.com>
2019-06-21 17:35:31 -0300

    fixed package.json and created Icon

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: path.join(__dirname, 'icons/Icon.icns'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
